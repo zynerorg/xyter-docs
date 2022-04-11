2a84dcd6b63e2a63a699b393e21d8173b1bc5467
Restyled.io <commits@restyled.io>
2022-04-11 15:56:29 +0000

    Restyled by prettier

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,118 +1,117 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title : 'Xyter',
-  tagline : 'An multi-purpose bot',
-  url : 'https://xyter.zyner.org',
-  baseUrl : '/',
-  projectName : 'xyter-docs',
-  organizationName : 'ZynerOrg',
-  trailingSlash : false,
-  onBrokenLinks : 'throw',
-  onBrokenMarkdownLinks : 'warn',
-  favicon : 'img/logo.png',
+  title: "Xyter",
+  tagline: "An multi-purpose bot",
+  url: "https://xyter.zyner.org",
+  baseUrl: "/",
+  projectName: "xyter-docs",
+  organizationName: "ZynerOrg",
+  trailingSlash: false,
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/logo.png",
 
-  presets : [
+  presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        docs : {
-          sidebarPath : require.resolve('./sidebars.js'),
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl :
-              'https://github.com/ZynerOrg/xyter-docs/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl:
+            "https://github.com/ZynerOrg/xyter-docs/tree/main/packages/create-docusaurus/templates/shared/",
         },
-        blog : {
-          showReadingTime : true,
+        blog: {
+          showReadingTime: true,
           // Please change this to your repo.
-          editUrl :
-              'https://github.com/ZynerOrg/xyter-docs/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl:
+            "https://github.com/ZynerOrg/xyter-docs/tree/main/packages/create-docusaurus/templates/shared/",
         },
-        theme : {
-          customCss : require.resolve('./src/css/custom.css'),
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
   ],
 
-  themeConfig :
-      /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-      ({
-        navbar : {
-          title : 'Xyter',
-          logo : {
-            alt : 'Xyter Logo',
-            src : 'img/logo.png',
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: "Xyter",
+        logo: {
+          alt: "Xyter Logo",
+          src: "img/logo.png",
+        },
+        items: [
+          {
+            type: "doc",
+            docId: "index",
+            position: "left",
+            label: "Documentation",
           },
-          items : [
-            {
-              type : 'doc',
-              docId : 'index',
-              position : 'left',
-              label : 'Documentation',
-            },
-            {to : '/blog', label : 'Blog', position : 'left'},
-            {
-              href : 'https://bot.zyner.org',
-              label : 'Invite our bot',
-              position : 'right',
-            },
-            {
-              href : 'https://github.com/ZynerOrg/xyter',
-              label : 'GitHub',
-              position : 'right',
-            },
-          ],
-        },
-        footer : {
-          style : 'dark',
-          links : [
-            {
-              title : 'Documentation',
-              items : [
-                {
-                  label : 'On-Premise',
-                  to : '/docs/on-premise',
-                },
-              ],
-            },
-            {
-              title : 'Community',
-              items : [
-                {
-                  label : 'Discord',
-                  href : 'https://discord.zyner.org',
-                },
-              ],
-            },
-            {
-              title : 'More',
-              items : [
-                {
-                  label : 'Blog',
-                  to : '/blog',
-                },
-                {
-                  label : 'GitHub',
-                  href : 'https://github.com/ZynerOrg/xyter',
-                },
-              ],
-            },
-          ],
-          copyright : `Copyright © ${
-              new Date().getFullYear()} Zyner.org. Built with Docusaurus.`,
-        },
-        prism : {
-          theme : lightCodeTheme,
-          darkTheme : darkCodeTheme,
-        },
-      }),
+          { to: "/blog", label: "Blog", position: "left" },
+          {
+            href: "https://bot.zyner.org",
+            label: "Invite our bot",
+            position: "right",
+          },
+          {
+            href: "https://github.com/ZynerOrg/xyter",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Documentation",
+            items: [
+              {
+                label: "On-Premise",
+                to: "/docs/on-premise",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Discord",
+                href: "https://discord.zyner.org",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "Blog",
+                to: "/blog",
+              },
+              {
+                label: "GitHub",
+                href: "https://github.com/ZynerOrg/xyter",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Zyner.org. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
 };
 
 module.exports = config;
--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,13 +1,13 @@
-import Link from '@docusaurus/Link';
-import clsx from 'clsx';
-import React from 'react';
+import Link from "@docusaurus/Link";
+import clsx from "clsx";
+import React from "react";
 
-import styles from './styles.module.css';
+import styles from "./styles.module.css";
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: "Easy to Use",
+    Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
     description: (
       <>
         Xyter is built to provide you with one bot for your whole guild. <br />
@@ -17,8 +17,8 @@ const FeatureList = [
     ),
   },
   {
-    title: 'Built with Typescript',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: "Built with Typescript",
+    Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
     description: (
       <>
         <b>We ❤️ open source</b> and Typescript makes it easier to understand
@@ -27,12 +27,12 @@ const FeatureList = [
     ),
   },
   {
-    title: 'On-premise is available',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: "On-premise is available",
+    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
     description: (
       <>
         If you would like to run it on-premise instead of us hosting it for you,
-        then you can follow our{' '}
+        then you can follow our{" "}
         <Link className="text--warning" to="https://github.com/ZynerOrg/xyter">
           this guide
         </Link>
@@ -42,9 +42,9 @@ const FeatureList = [
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description }) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
@@ -63,9 +63,9 @@ export default function HomepageFeatures() {
         <div className="row">
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
-          ))
-}
-</div>
-      </div>< /section>
+          ))}
+        </div>
+      </div>
+    </section>
   );
 }
