49f3528aa3b5737c7f0b645da56a3bf67a77b32c
Vermium Sifell <vermium@zyner.org>
2022-04-11 18:12:55 +0200

    📝 changed title and description

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,10 +29,7 @@ function HomepageHeader() {
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
-    >
+    <Layout title={`Homepage`} description="An multi-purpose discord.js bot">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
