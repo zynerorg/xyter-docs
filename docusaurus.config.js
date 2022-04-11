ab892a3c5a7783042e1381adf04fba8709c77543
Vermium Sifell <vermium@zyner.org>
2022-04-11 17:53:12 +0200

    🔧 add correct url in config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Xyter',
   tagline: 'An multi-purpose bot',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://xyter.zyner.org',
   baseUrl: '/',
   projectName: 'xyter-docs',
   organizationName: 'ZynerOrg',
