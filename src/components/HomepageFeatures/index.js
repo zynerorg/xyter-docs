d0a62c74cad294cad48a7a158019a49a7a9dcf6c
Restyled.io <commits@restyled.io>
2022-05-20 08:01:58 +0000

    Restyled by prettier

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -10,7 +10,8 @@ const FeatureList = [
     Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
     description: (
       <>
-        Xyter is built for privacy and simplicity. By replacing all bots with one will make guilds less cluttered and easier to manage.
+        Xyter is built for privacy and simplicity. By replacing all bots with
+        one will make guilds less cluttered and easier to manage.
       </>
     ),
   },
@@ -19,7 +20,8 @@ const FeatureList = [
     Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
     description: (
       <>
-        By using TypeScript, the code is easier to understand and contribute to, bug fixes are easier to find too.
+        By using TypeScript, the code is easier to understand and contribute to,
+        bug fixes are easier to find too.
       </>
     ),
   },
@@ -28,10 +30,11 @@ const FeatureList = [
     Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
     description: (
       <>
-        If you would like to run it on-premise instead of <Link className="text--warning" to="https://bot.zyner.org">
+        If you would like to run it on-premise instead of{" "}
+        <Link className="text--warning" to="https://bot.zyner.org">
           us hosting it for you
-        </Link>,
-        then you can follow our{" "}
+        </Link>
+        , then you can follow our{" "}
         <Link className="text--warning" to="https://github.com/ZynerOrg/xyter">
           this guide
         </Link>
@@ -41,7 +44,7 @@ const FeatureList = [
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description }) {
   return (
     <div className={clsx("col col--4")}>
       <div className="text--center">
@@ -62,9 +65,9 @@ export default function HomepageFeatures() {
         <div className="row">
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
-          ))
-}
-</div>
-      </div>< /section>
+          ))}
+        </div>
+      </div>
+    </section>
   );
 }
