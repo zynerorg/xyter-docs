1223a864730979a380fabab447e8c61c00b0131a
Restyled.io <commits@restyled.io>
2022-10-22 11:56:41 +0000

    Restyled by clang-format

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import React from 'react';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
+  const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
@@ -16,7 +16,7 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
-            className="button button--secondary button--lg"
+  className = "button button--secondary button--lg"
             to="/docs/hosting"
           >
             Getting Started
@@ -28,13 +28,9 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <Layout title={`Homepage`} description="A multi-purpose discord.js bot.">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+  const {siteConfig} = useDocusaurusContext();
+  return (<Layout title = {`Homepage`} description =
+               "A multi-purpose discord.js bot."><HomepageHeader /><main>
+          <HomepageFeatures /></main>
+    </Layout>);
 }
