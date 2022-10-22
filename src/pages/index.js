b5cdbe49c41f6449fb3c72b7551e5bbb7a2fdb86
Restyled.io <commits@restyled.io>
2022-10-22 11:56:45 +0000

    Restyled by prettier

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,22 @@
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Layout from '@theme/Layout';
-import clsx from 'clsx';
-import React from 'react';
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import Layout from "@theme/Layout";
+import clsx from "clsx";
+import React from "react";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
-  className = "button button--secondary button--lg"
+            className="button button--secondary button--lg"
             to="/docs/hosting"
           >
             Getting Started
@@ -28,9 +28,13 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
-  return (<Layout title = {`Homepage`} description =
-               "A multi-purpose discord.js bot."><HomepageHeader /><main>
-          <HomepageFeatures /></main>
-    </Layout>);
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout title={`Homepage`} description="A multi-purpose discord.js bot.">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
 }
